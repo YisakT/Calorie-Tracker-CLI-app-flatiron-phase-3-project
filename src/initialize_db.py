62bb9705bd54e2617734afa7ad654a6b8bcb70d4
Yisak <yisakbruck23@gmail.com>
2023-08-28 21:01:51 -0400

    Set up database models for calorie tracker.

--- src/initialize_db.py
+++ src/initialize_db.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from models import Base
-import config
 
-engine = create_engine(config.DATABASE_URL)
+DATABASE_URI = 'sqlite:///calories_tracker.db' 
+engine = create_engine(DATABASE_URI)
+
+# Create tables
 Base.metadata.create_all(engine)
--- src/models.py
+++ src/models.py
@@ -1,25 +1,36 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 import datetime
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'users'
+class FoodItem(Base):
+    __tablename__ = 'food_items'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    age = Column(Integer)
-    weight = Column(Float)
-    meals = relationship('Meal', back_populates='user')
+    name = Column(String, unique=True, nullable=False)
+    calories = Column(Integer, nullable=False)
+
+    # Relationship with MealFood association table
+    meals = relationship('MealFood', back_populates='food_item')
 
 class Meal(Base):
     __tablename__ = 'meals'
 
     id = Column(Integer, primary_key=True)
-    description = Column(String)
-    calories = Column(Float)
-    date = Column(DateTime, default=datetime.datetime.utcnow)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship('User', back_populates='meals')
+    name = Column(String, unique=True, nullable=False)
+
+    # Relationship with MealFood association table
+    food_items = relationship('MealFood', back_populates='meal')
+
+# Association table for many-to-many relationship
+class MealFood(Base):
+    __tablename__ = 'meal_food'
+
+    meal_id = Column(Integer, ForeignKey('meals.id'), primary_key=True)
+    food_item_id = Column(Integer, ForeignKey('food_items.id'), primary_key=True)
+
+    # Relationship to the above tables
+    meal = relationship('Meal', back_populates='food_items')
+    food_item = relationship('FoodItem', back_populates='meals')
